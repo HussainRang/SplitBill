1d4e575136c2e3961c8b20c9c897e5c7063b9415
HussainRang <rangwalahussain11@gmail.com>
2022-05-21 02:13:36 +0530

    interface_1 half_1

---
+++ app/src/main/java/com/example/splitbill/DataBaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.splitbill;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+import java.util.List;
+
+
+class DataBaseHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "SplitBill";
+
+    public DataBaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        //3rd argument to be passed is CursorFactory instance
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_MAIN_TABLE = "CREATE TABLE SplitBillMain (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                    "tripName TEXT," +
+                                    "num_mem INT)";
+        db.execSQL(CREATE_MAIN_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS SplitBillMain");
+
+        // Create tables again
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/splitbill/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.splitbill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DataBaseHandler dbh = new DataBaseHandler(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void Main_Add_Button_pressed(View view)
+    {
+        //change intent to add activity name and add members page
+
+        //Send an arraylist having { id, group_name,number_of_members } or (int id,String group_name,int number_of_members)
+        add_frame_main_interface();
+    }
+
+    public void add_frame_main_interface() {
+
+        LinearLayout ll = findViewById(R.id.ll_addfr_main_interface);
+
+        //creating frame dynamically
+        FrameLayout fr = new FrameLayout(this);
+        //layout params for frames
+        FrameLayout.LayoutParams params_fr = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT,
+                Gravity.CENTER);
+        fr.setLayoutParams(params_fr);
+        //frames can be clicked
+        fr.setClickable(true);
+        //GIVING TAG TO FRAME
+        //fr.setTag("FRAME"+id);
+        //SETTING ONCLICK FUNCTION
+        fr.setOnClickListener(frame_clicked(fr));
+
+        // creating horizontal linear layout dynamically
+        LinearLayout lr_hor = new LinearLayout(this);
+        //setting layout parameters
+        LinearLayout.LayoutParams params_lr_hor = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        lr_hor.setLayoutParams(params_lr_hor);
+        //setting orientation as horizontal
+        lr_hor.setOrientation(LinearLayout.HORIZONTAL);
+
+        //add image view
+
+        // adding vertical linear layout dynamically
+        LinearLayout lr_ver = new LinearLayout(this);
+        //setting layout params
+        LinearLayout.LayoutParams params_lr_ver = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        lr_ver.setLayoutParams(params_lr_ver);
+        //setting orientation as vertical
+        lr_ver.setOrientation(LinearLayout.VERTICAL);
+
+
+        //adding text view to add GROUP NAME
+        TextView tv1 = new TextView(this);
+        tv1.setText("Group Name");
+        LinearLayout.LayoutParams params_tv1 = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        tv1.setLayoutParams(params_tv1);
+        //Text Size
+        tv1.setTextSize(12);
+        //textview Orientation
+        tv1.setGravity(Gravity.LEFT);
+
+        //TextView to add number of members in group
+        TextView tv2 = new TextView(this);
+        tv2.setText("Members");
+        LinearLayout.LayoutParams params_tv2 = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        tv2.setLayoutParams(params_tv2);
+        //setting text size
+        tv2.setTextSize(6);
+        //textView orientation
+        tv2.setGravity(Gravity.LEFT);
+
+
+
+        //Adding views to vertical linear layout
+        lr_ver.addView(tv1);
+        lr_ver.addView(tv2);
+        //Adding views to horizontal linear layout
+        lr_hor.addView(lr_ver);
+        //adding views to frame
+        fr.addView(lr_hor);
+        //adding views to scrollview
+        Toast.makeText(getApplicationContext(),"Frame Added",Toast.LENGTH_SHORT).show();
+        ll.addView(fr);
+
+    }
+    
+    public View.OnClickListener frame_clicked(View view)
+    {
+
+        return null;
+    }
+
+}
